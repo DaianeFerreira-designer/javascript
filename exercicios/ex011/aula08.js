8b892294471278f013ce17f082c0d2426295f445
DaianeFerreira-designer <161055790+DaianeFerreira-designer@users.noreply.github.com>
2024-05-05 14:39:25 -0300

    ex

---
+++ exercicios/ex011/aula08.js
@@ -0,0 +1,6 @@
+var idade = 30
+console.log(`Sua idade é ${idade}`)
+if (idade > 18){
+    console.log('você é maior de idade, então pode beber')
+}
+console.log('Beba com moderação')
---
+++ exercicios/ex011/aula09.js
@@ -0,0 +1,9 @@
+var idade = 18
+console.log(`Sua idade é ${idade}`)
+if (idade > 18){
+    console.log(`você é maior de idade, tem ${idade} então, pode beber`)
+    console.log('Beba com moderação')
+} else {
+console.log(`você é menor de idade, tem ${idade} então, não pode beber`)
+console.log('Não Beba')
+}
